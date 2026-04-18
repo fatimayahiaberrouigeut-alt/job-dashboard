ecd0a316cd049d17800d681239645416206ac5a4
fatimayahiaberrouigeut-alt <fatimayahiaberrouigeut@gmail.com>
2026-04-18 11:19:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,19 @@
 import streamlit as st
+import pandas as pd
 
-st.title("Mon Dashboard 🚀")
-st.write("Ça marche !")
+st.title("📊 Job Market Dashboard")
+
+# lire le fichier
+df = pd.read_csv("emploitic (2).csv")
+
+# afficher nombre total
+st.metric("Total Jobs", len(df))
+
+# afficher tableau
+st.subheader("📄 Données")
+st.dataframe(df)
+
+# top titres (si colonne existe)
+if "title" in df.columns:
+    st.subheader("📈 Top Jobs")
+    st.bar_chart(df["title"].value_counts().head(10))
