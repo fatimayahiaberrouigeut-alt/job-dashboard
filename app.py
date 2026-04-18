4ef8206c94abeae59575d72baef8d704d1fc01a0
fatimayahiaberrouigeut-alt <fatimayahiaberrouigeut@gmail.com>
2026-04-18 11:28:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 
     st.write(df.columns)
-    st.subheader("🧠 Insights")
-
-if "title" in df.columns:
-    top_job = df["title"].value_counts().idxmax()
-    st.write(f"Le job le plus demandé est : **{top_job}**")
+   
